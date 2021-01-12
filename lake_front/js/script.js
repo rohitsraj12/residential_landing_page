da5df46a981ae23177dbbf4c815142efa8d89316
Rohit <rohitsraj12@gmail.com>
2021-01-12 14:15:22 +0530

    Update script.js

--- lake_front/js/script.js
+++ lake_front/js/script.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
     $("#hamburger").click(function(){
         $("#header-nav").slideToggle();
     })
+
+    
+    // scroll down fixed body header 
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 600) {
+            $("#body-header").addClass("header__fixed");
+        } else {
+            $("#body-header").removeClass("header__fixed");
+        }
+    });
 })
