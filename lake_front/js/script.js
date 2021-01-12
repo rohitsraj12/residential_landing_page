01832f6e20cae8c352891b5d832b633114f2276e
Rohit <rohitsraj12@gmail.com>
2021-01-12 16:19:46 +0530

    Update script.js

--- lake_front/js/script.js
+++ lake_front/js/script.js
@@ -9,19 +9,11 @@ $(document).ready(function(){
         var scroll = $(window).scrollTop();
     
         if (scroll >= 600) {
-            // $("#body-header").addClass("header__fixed");
-            $("#body-header").css({
-                width: '100%',
-                position: 'fixed',
-                background: "rgba(255, 255, 255, 0.96)",
-                'z-index': '99'
-            });
+            $("#body-header").addClass("header__fixed");
+            
         } else {
-            // $("#body-header").removeClass("header__fixed");
-            $("#body-header").css({
-                position: 'absolute',
-                background: 'rgba(255, 255, 255, 0.87)'
-            });
+            $("#body-header").removeClass("header__fixed");
+          
         }
     });
 })
