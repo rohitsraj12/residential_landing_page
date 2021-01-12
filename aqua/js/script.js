807ec82b537a4a099221b68c7c8367a4d7b68b69
Rohit <rohitsraj12@gmail.com>
2021-01-12 14:35:14 +0530

    Update script.js

--- aqua/js/script.js
+++ aqua/js/script.js
@@ -9,9 +9,19 @@ $(document).ready(function(){
         var scroll = $(window).scrollTop();
     
         if (scroll >= 600) {
-            $("#body-header").addClass("header__fixed");
+            // $("#body-header").addClass("header__fixed");
+            $("#body-header").css({
+                width: '100%',
+                position: 'fixed',
+                background: "rgba(255, 255, 255, 0.96)",
+                'z-index': '99'
+            });
         } else {
-            $("#body-header").removeClass("header__fixed");
+            // $("#body-header").removeClass("header__fixed");
+            $("#body-header").css({
+                position: 'absolute',
+                background: 'rgba(255, 255, 255, 0.87)'
+            });
         }
     });
 })
