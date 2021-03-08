2bed49b5024dc4f8720b97bdb57da558e08645aa
Rohit <rohitsraj12@gmail.com>
2021-03-08 17:36:18 +0530

    Update contact_form.php

--- landing_page_tempates/march/contact_form.php
+++ landing_page_tempates/march/contact_form.php
@@ -7,7 +7,9 @@ if(isset($_POST['submit_contact'])){
     $user_email = $_POST['email'];
     $user_message = $_POST['message'];
 
+    // email to in house
     $to = "webdesigner@gopalanenterprises.com";
+    $to2 = "seo@gopalanenterprises.com";
     $subject = "Contact Us Form | Gopalan "+ $project_name +" Landing Page";
     $message = "<p>Enquiry</p></br>";
     $message .= "<p> name: <strong>". $user_name ."</strong></p></br>";
@@ -23,6 +25,23 @@ if(isset($_POST['submit_contact'])){
     $headers .= "Content-type: text/html\r\n";
 
     mail($to, $subject, $message, $headers);
+    mail($to2, $subject, $message, $headers);
+
+    // email to user
+    $u_subject = "Thank You For The Enquiry Of Gopalan " + $project_name;
+    $u_message = "<p>Dear</p> "+ $user_name +"</br>";
+    $u_message .= "<p> Thanks message.</p></br>";
+    $u_message .= "<p style='background-image: url();'> Thanks message.</p></br>";
+    $u_message .= "<p>Thank you,</p>";
+    $u_message .= "<p>". $user_name ."</p>";
+    $u_message .= "<p>" . $user_email . " | " . $user_phone . "</p>";
+    
+    $u_headers = "From:" . $user_name . " <" . $user_email . ">\r\n";
+    $u_headers .= "Reply-To: " . $user_email . "\r\n";
+    $u_headers .= "Content-type: text/html\r\n";
+
+    mail($user_email, $u_subject, $u_message, $u_headers);
+
 
     header("location:thank-you.html");
 }
